03005ef2a068db8cbc6347ca42432d132d693808
eugustavonsc <gustavo.slv1516@gmail.com>
2024-03-17 15:40:02 -0300

    algumas alteracoes

--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 class Category (models.Model):
     name= models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
 
 
 
@@ -18,9 +20,11 @@ class Recipe (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/',blank=True, default='')
     category= models.ForeignKey(Category, on_delete= models.SET_NULL, null=True)
     author= models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
+    def __str__(self):
+        return self.title
 
 # EDITED
 # title description slug
